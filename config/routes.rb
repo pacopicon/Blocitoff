b56a9ab845fff37174a3b8c6755ee00c3c75a91e
Paco <palmtreerooskee@gmail.com>
2015-11-25 14:04:54 -0600

    4th commit, updated Gemfile for deployment to Heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "welcome#index"
+  root "applciation#hello"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
